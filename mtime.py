042ae12aab5304f3bb144331dd9a45a166702274
배규태 <gt.bae@piolink.com>
2019-01-09 17:26:51 +0900

    Python Monotonic Timer
    import mtime
    t = mtime.time() # t is monotonic time. It's always increase.

---
+++ mtime.py
@@ -0,0 +1,24 @@
+import ctypes
+import os
+
+__all__ = ['time']
+CLOCK_MONOTONIC_RAW = 4
+
+
+class timespec(ctypes.Structure):
+    _fields_ = [
+        ('tv_sec', ctypes.c_long),
+        ('tv_nsec', ctypes.c_long)
+    ]
+
+librt = ctypes.CDLL('librt.so.1', use_errno=True)
+clock_gettime = librt.clock_gettime
+clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
+
+
+def time():
+    t = timespec()
+    if clock_gettime(CLOCK_MONOTONIC_RAW, ctypes.pointer(t)) != 0:
+        errno_ = ctypes.get_errno()
+        raise OSError(errno_, os.strerror(errno_))
+    return t.tv_sec + (t.tv_nsec * 1e-9)
